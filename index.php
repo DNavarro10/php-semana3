387162becb9023c463b84bf7bcc8e398e5081d33
DNavarro10 <luis_d_100@hotmail.com>
2017-06-21 23:21:32 -0500

    Inicio
    semana 3 del php.  POO ll

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Semana 3 POO II</title>
+	<link rel="stylesheet" href="css/Style.css">
+	
+</head>
+
+<body>
+<div class="contenedor">
+		<div class="titulo">
+			<header>
+				<div><h1>Curso de PHP - Semana 3</h1></div>
+				<div><p>Programacion orientada a objetos II</p></div>
+				
+			</header>
+		</div>
+		<div class="main">
+			<div class="php">
+				<div class="warp">
+					<?php include 'php/trabajadores.php'; ?>	
+				
+				</div>
+	
+	
+			</div>
+		</div>
+		<footer>
+			<div>
+				<p>
+					Diego Navarro - PHP 2017
+				</p>
+			</div>
+		</footer>
+	</div>
+</body>
+
+</html>
